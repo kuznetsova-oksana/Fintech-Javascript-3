58205b757d72d47f8d4e313daf961c1e84e9a016
Oksana Kuznetsova <kuznetsova.oksana.w@gmail.com>
2017-10-12 10:07:49 +0300

    extend numbers behavior

--- 03/5/task.js
+++ 03/5/task.js
@@ -6,3 +6,35 @@
  * 0..X => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
  * Подсказка - необходимо использовать Proxy - объекты
  * */
+
+const romanNumbersTable = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+function toArabicNumber(romanNumber) {
+  let number = 0;
+  let index = 0;
+
+  while (index < romanNumber.length) {
+    const curLetter = romanNumber[index];
+    const nextLetter = romanNumber[index + 1];
+    const curArabic = romanNumbersTable[curLetter];
+    const nextArabic = romanNumbersTable[nextLetter];
+
+    if (nextArabic > curArabic) {
+      number += nextArabic - curArabic;
+      index += 2;
+    } else {
+      number += curArabic;
+      index += 1;
+    }
+  }
+
+  return number;
+}
+
+const targetPrototype = Object.getPrototypeOf(Number.prototype);
+
+const proxy = new Proxy(targetPrototype, {
+  get: (target, name) => (name in target ? target.name : [...new Array(toArabicNumber(name)).keys()])
+});
+
+Object.setPrototypeOf(Number.prototype, proxy);
